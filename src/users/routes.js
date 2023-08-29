96cc3bc0327720fabafba03c0a2737ebb957531e
Benjamin Howe <yefool@hotmail.co.uk>
2023-08-29 10:06:27 +0100

    first commit

--- src/users/routes.js
+++ src/users/routes.js
@@ -3,14 +3,14 @@ const userRouter = Router();
 
 
 const {registerUser, getAllUsers, deleteUser, updateUser, userLogin} = require("./controllers");
-const { hashPass, comparePass, tokenCheck, tokenDeleteCheck } = require("../middleware");
+const { hashPass, comparePass, tokenCheck } = require("../middleware");
 
 
 
 userRouter.post("/users/registerUser", hashPass, registerUser);
 userRouter.get("/users/getAllUsers", tokenCheck, getAllUsers); //protected route
 userRouter.put("/users/updateUser", updateUser);
-userRouter.delete("/users/deleteUser", tokenDeleteCheck, deleteUser);
+userRouter.delete("/users/deleteUser", deleteUser);
 userRouter.post("/users/userLogin", comparePass, userLogin);
 
 userRouter.get("/users/authCheck", tokenCheck, userLogin)
